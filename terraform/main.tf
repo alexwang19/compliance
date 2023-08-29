7177db90d8a84bf5dc6edefb65e8a4f5a8489d33
alexwang19 <120605051+alexwang19@users.noreply.github.com>
2023-08-29 10:18:35 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket_public_access_block" "sko_bucket" {
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
-  restrict_public_buckets = false
+  restrict_public_buckets = true
 }
 
 /*
